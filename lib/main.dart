05f5e9784cf57563e7e26bb636c839430865ea97
saidur-rahaman <jahir15-4892@diu.edu.bd>
2025-02-19 11:13:28 +0600

    Task 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,29 +17,29 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue[400],
           centerTitle: true,
         ),
-        body: Row(
-          children: [
-            Icon(
-              Icons.star,
-              color: Colors.yellow,
-              size: 60,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.yellow,
-              size: 60,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.grey,
-              size: 60,
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.yellow,
-              size: 60,
-            ),
-          ],
+        body: Container(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 50,
+              ),
+              Image(
+                image: AssetImage("assets/aa.png"), // Ensure path is correct
+                width: 100,
+                height: 100,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 50,
+              ),
+            ],
+          ),
         ),
       ),
     );
