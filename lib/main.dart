1e0ba816a71baa8cc3b907f99d02eb2b456727b5
saidur-rahaman <jahir15-4892@diu.edu.bd>
2025-02-19 10:15:33 +0600

    Task 1.3

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,11 @@ class MyApp extends StatelessWidget {
             ),
             Icon(
               Icons.star,
+              color: Colors.grey,
+              size: 60,
+            ),
+            Icon(
+              Icons.star,
               color: Colors.yellow,
               size: 60,
             ),
